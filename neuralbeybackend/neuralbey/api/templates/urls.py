c41f8866cbed35bd336d9054857e8663ef5bd7ad
ALI0benjannet <alialilou54098546@gmail.com>
2025-08-06 21:06:43 +0100

    current working version

--- neuralbeybackend/neuralbey/api/templates/urls.py
+++ neuralbeybackend/neuralbey/api/templates/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
+# Création du router DRF
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet)
+router.register(r'tasks', TaskViewSet, basename='task')
 
+# Inclusion des URLs générées par le router
 urlpatterns = [
     path('', include(router.urls)),
 ]
